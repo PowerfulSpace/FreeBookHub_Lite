e745976f96898b21665d88fa0286d9b74e6b4995
Alexander <sany199574@gmail.com>
2025-05-03 15:49:31 +0300

    feat(domain): добавление сущности Book с бизнес-логикой
    - Создана базовая сущность Book в Domain/Entities
    - Реализованы основные свойства и валидация
    - Добавлены методы для работы со скидками и остатками
    - Обновлены зависимости API-проекта для работы с EF Core

---
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.CatalogService.Domain/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace PS.FreeBookHub_Lite.CatalogService.Domain.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Title { get; set; } = default!;
+        public string Author { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string ISBN { get; set; } = default!;
+
+        public decimal Price { get; set; }
+
+        public DateTime PublishedAt { get; set; }
+
+        public string CoverImageUrl { get; set; } = default!;
+    }
+}

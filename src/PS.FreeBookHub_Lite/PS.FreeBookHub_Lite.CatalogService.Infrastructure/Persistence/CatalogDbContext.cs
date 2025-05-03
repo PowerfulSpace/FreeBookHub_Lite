c0e9fa8742b63e0aea27795808a8b4181fa50057
Alexander <sany199574@gmail.com>
2025-05-04 00:02:08 +0300

    feat(infrastructure): добавлен слой Persistence и обновлен .csproj
    - Добавлена папка Persistence для управления данными в слое Infrastructure
    - Реализованы базовые компоненты для работы с данными
    - Обновлены зависимости проекта в `Infrastructure.csproj`

---
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.CatalogService.Infrastructure/Persistence/CatalogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PS.FreeBookHub_Lite.CatalogService.Domain.Entities;
+
+namespace PS.FreeBookHub_Lite.CatalogService.Infrastructure.Persistence
+{
+
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books => Set<Book>();
+
+    }
+}
---
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.CatalogService.Infrastructure/Persistence/Repositories/BookRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PS.FreeBookHub_Lite.CatalogService.Application.Interfaces;
+using PS.FreeBookHub_Lite.CatalogService.Domain.Entities;
+
+namespace PS.FreeBookHub_Lite.CatalogService.Infrastructure.Persistence.Repositories
+{
+    public class EfBookRepository : IBookRepository
+    {
+        private readonly CatalogDbContext _context;
+
+        public EfBookRepository(CatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book?> GetByIdAsync(Guid id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task AddAsync(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Book book)
+        {
+            _context.Books.Update(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book is not null)
+            {
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}

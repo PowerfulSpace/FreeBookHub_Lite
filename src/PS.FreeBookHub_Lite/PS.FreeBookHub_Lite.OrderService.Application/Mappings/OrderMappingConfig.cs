a9cde9363609ace7062243823b6c700976ee41a6
Alexander <sany199574@gmail.com>
2025-05-14 23:41:00 +0300

    feat : Добавление мапигна  - OrderMappingConfig

---
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.OrderService.Application/Mappings/OrderMappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using PS.FreeBookHub_Lite.OrderService.Application.DTOs;
+
+namespace PS.FreeBookHub_Lite.OrderService.Application.Mappings
+{
+    public class OrderMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<CreateOrderRequest, OrderDto>();
+            config.NewConfig<CreateOrderItemRequest, OrderItemDto>();
+        }
+    }
+}

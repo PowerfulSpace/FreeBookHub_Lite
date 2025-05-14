ff4f0e10f8a4901fa9e393e8737d48c0db704f64
Alexander <sany199574@gmail.com>
2025-05-15 00:01:05 +0300

    feat: Добавлены валидаторы в Application сервиса заказов  - CreateOrderItemRequestValidator  - CreateOrderRequestValidator

---
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.OrderService.Application/Validators/CreateOrderItemRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using PS.FreeBookHub_Lite.OrderService.Application.DTOs;
+
+namespace PS.FreeBookHub_Lite.OrderService.Application.Validators
+{
+    public class CreateOrderItemRequestValidator : AbstractValidator<CreateOrderItemRequest>
+    {
+        public CreateOrderItemRequestValidator()
+        {
+            RuleFor(x => x.BookId)
+                 .NotEmpty().WithMessage("BookId is required.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than 0.");
+
+            RuleFor(x => x.UnitPrice)
+                .GreaterThan(0).WithMessage("Unit price must be greater than 0.");
+        }
+    }
+}
---
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.OrderService.Application/Validators/CreateOrderRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using PS.FreeBookHub_Lite.OrderService.Application.DTOs;
+
+namespace PS.FreeBookHub_Lite.OrderService.Application.Validators
+{
+    public class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
+    {
+        public CreateOrderRequestValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("UserId is required.");
+
+            RuleFor(x => x.ShippingAddress)
+                .NotEmpty().WithMessage("Shipping address is required.")
+                .MaximumLength(200).WithMessage("Shipping address cannot exceed 200 characters.");
+
+            RuleFor(x => x.Items)
+                .NotEmpty().WithMessage("Order must contain at least one item.");
+
+            RuleForEach(x => x.Items)
+                .SetValidator(new CreateOrderItemRequestValidator());
+        }
+    }
+}

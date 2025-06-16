649cec637b7af16ca5e85c9b2773576fb907a494
Alexander <sany199574@gmail.com>
2025-06-17 00:05:15 +0300

    refactor(payment-service): обновление логирования в LoggerMessages
    - Оптимизирован `LoggerMessages.cs`, добавлены новые лог-сообщения для более точного мониторинга платежных операций.

--- src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.PaymentService.Common/LoggerMessages.cs
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.PaymentService.Common/LoggerMessages.cs
@@ -2,28 +2,30 @@
 {
     public static class LoggerMessages
     {
-        // Payment Errors
+        // ExceptionHandlingMiddleware
+        //                  --- Payment Errors
         public const string PaymentNotFound = "Payment not found — PaymentId: {PaymentId} | Method: {Method} | Path: {Path}";
         public const string UnauthorizedAccess = "Unauthorized access — PaymentId: {PaymentId}, UserId: {UserId} | Method: {Method} | Path: {Path}";
         public const string DuplicatePayment = "Duplicate payment — OrderId: {OrderId} | Method: {Method} | Path: {Path}";
         public const string InvalidPaymentStatus = "Invalid payment status — Current: {CurrentStatus}, Required: {RequiredStatus} | Method: {Method} | Path: {Path}";
         public const string InvalidUserIdentifier = "Invalid user identifier — InvalidId: {InvalidId} | Method: {Method} | Path: {Path}";
 
-        // General Error
+        //                  --- General Error Handling
         public const string UnhandledException = "Unhandled exception — Message: {Message} | Method: {Method} | Path: {Path}";
 
-        // ProcessPaymentAsync
+
+        // PaymentBookService
+        //                  --- ProcessPaymentAsync
         public const string ProcessPaymentStarted = "Processing payment — OrderId: {OrderId}, UserId: {UserId}";
         public const string ProcessPaymentSuccess = "Payment processed — PaymentId: {PaymentId}, OrderId: {OrderId}";
 
-        // GetPaymentByIdAsync
+        //                  --- GetPaymentByIdAsync
         public const string GetPaymentByIdStarted = "Fetching payment — PaymentId: {PaymentId}";
         public const string GetPaymentByIdSuccess = "Payment retrieved — PaymentId: {PaymentId}";
 
-        // GetPaymentsByOrderIdAsync
+        //                  --- GetPaymentsByOrderIdAsync
         public const string GetPaymentsByOrderStarted = "Fetching payments — OrderId: {OrderId}";
         public const string GetPaymentsByOrderSuccess = "Payments retrieved — OrderId: {OrderId}, Count: {Count}";
-
     }
 
 }

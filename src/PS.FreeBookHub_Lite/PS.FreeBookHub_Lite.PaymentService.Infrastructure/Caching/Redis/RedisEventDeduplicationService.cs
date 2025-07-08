044965e72e1bf8745757611bd6517b07626d37ae
Alexander <sany199574@gmail.com>
2025-07-08 07:01:48 +0300

    refactor(redis): унификация логики дедупликации событий в OrderService и PaymentService
    - Обновлены `RedisEventDeduplicationService.cs` в обоих сервисах:
      - Повышена надёжность при обработке повторных событий.

--- src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.PaymentService.Infrastructure/Caching/Redis/RedisEventDeduplicationService.cs
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.PaymentService.Infrastructure/Caching/Redis/RedisEventDeduplicationService.cs
@@ -1,4 +1,5 @@
-﻿using PS.FreeBookHub_Lite.PaymentService.Application.Interfaces.Redis;
+﻿using Microsoft.Extensions.Logging;
+using PS.FreeBookHub_Lite.PaymentService.Application.Interfaces.Redis;
 using StackExchange.Redis;
 
 namespace PS.FreeBookHub_Lite.PaymentService.Infrastructure.Caching.Redis
@@ -6,14 +7,27 @@ namespace PS.FreeBookHub_Lite.PaymentService.Infrastructure.Caching.Redis
     public class RedisEventDeduplicationService : IEventDeduplicationService
     {
         private readonly IDatabase _database;
-        public RedisEventDeduplicationService(IConnectionMultiplexer redis)
+        private readonly ILogger<RedisEventDeduplicationService> _logger;
+        public RedisEventDeduplicationService(
+            IConnectionMultiplexer redis,
+            ILogger<RedisEventDeduplicationService> logger)
         {
             _database = redis.GetDatabase();
+            _logger = logger;
         }
 
         public async Task<bool> IsDuplicateAsync(string key, TimeSpan ttl, CancellationToken ct)
         {
-            return !await _database.StringSetAsync(key, "1", ttl, When.NotExists);
+            try
+            {
+                return !await _database.StringSetAsync(key, "1", ttl, When.NotExists);
+            }
+            catch (RedisConnectionException ex)
+            {
+                _logger.LogError(ex, "Redis connection failed");
+                throw new Exception("Redis unavailable", ex);
+                //throw new ServiceUnavailableException("Redis unavailable", ex);
+            }
         }
     }
 }

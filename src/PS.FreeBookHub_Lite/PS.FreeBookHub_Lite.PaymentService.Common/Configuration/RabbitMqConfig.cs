879e760514098b32334c6cef9155d281389b7491
Alexander <sany199574@gmail.com>
2025-07-14 00:11:20 +0300

    refactor(payment-service): очищен RabbitMqConfig от лишних комментариев

--- src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.PaymentService.Common/Configuration/RabbitMqConfig.cs
+++ src/PS.FreeBookHub_Lite/PS.FreeBookHub_Lite.PaymentService.Common/Configuration/RabbitMqConfig.cs
@@ -1,21 +1,5 @@
 ﻿namespace PS.FreeBookHub_Lite.PaymentService.Common.Configuration
 {
-    //public class RabbitMqConfig
-    //{
-    //    public string HostName { get; set; } = "localhost";
-    //    public string ExchangeName { get; set; } = "bookhub.exchange";
-    //    public string OrderCreatedQueue { get; set; } = "order.created";
-    //    public string OrderCreatedRoutingKey { get; set; } = "order.created";
-    //    public string PaymentCompletedRoutingKey { get; set; } = "payment.completed";
-
-    //    public string OrderCreatedDeadLetterExchange { get; set; } = "bookhub.exchange.dlx";
-    //    public string OrderCreatedDeadLetterQueue { get; set; } = "order.created.dlq";
-    //    public string OrderCreatedDeadLetterRoutingKey { get; set; } = "order.created.dlq";
-
-    //    public int RetryIntervalMs { get; set; } = 5000;
-    //    public int MaxRetryCount { get; set; } = 5;
-    //}
-
     public class RabbitMqConfig
     {
         public string HostName { get; set; } = "localhost";

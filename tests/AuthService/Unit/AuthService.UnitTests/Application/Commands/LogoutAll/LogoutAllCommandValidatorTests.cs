a7612105c2ff48c8732a0a66d8df8dd93a7cb0e0
Alexander <sany199574@gmail.com>
2025-07-23 22:57:58 +0300

    test(auth-service): добавлены unit-тесты валидаторов LogoutAll и RefreshToken команд
    - `LogoutAllCommandValidatorTests`: проверка валидности запроса на деактивацию всех токенов пользователя.
    - `RefreshTokenCommandValidatorTests`: тестирование валидности refresh-запроса, реакции на отсутствующие/невалидные токены.

---
+++ tests/AuthService/Unit/AuthService.UnitTests/Application/Commands/LogoutAll/LogoutAllCommandValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.TestHelper;
+using PS.FreeBookHub_Lite.AuthService.Application.CQRS.Commands.LogoutAll;
+
+namespace AuthService.UnitTests.Application.Commands.LogoutAll
+{
+    public class LogoutAllCommandValidatorTests
+    {
+        private readonly LogoutAllCommandValidator _validator = new();
+
+        [Fact]
+        public async Task Should_HaveValidationError_When_UserIdIsEmpty()
+        {
+            // Arrange
+            var model = new LogoutAllCommand { UserId = Guid.Empty };
+
+            // Act
+            var result = await _validator.TestValidateAsync(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.UserId)
+                  .WithErrorMessage("UserId is required.");
+        }
+
+        [Fact]
+        public async Task Should_NotHaveValidationError_When_UserIdIsValid()
+        {
+            // Arrange
+            var model = new LogoutAllCommand { UserId = Guid.NewGuid() };
+
+            // Act
+            var result = await _validator.TestValidateAsync(model);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.UserId);
+        }
+    }
+}

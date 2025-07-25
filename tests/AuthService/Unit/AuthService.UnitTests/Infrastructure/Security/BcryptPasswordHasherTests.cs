694827af7386aa5a46601a84e9db166827a5bfb8
Alexander <sany199574@gmail.com>
2025-07-25 04:00:46 +0300

    test(auth-security): добавлены unit-тесты для инфраструктурной части Security
    - Подготовлена директория `Security/` для тестов провайдеров шифрования, хэширования, и управления токенами.

---
+++ tests/AuthService/Unit/AuthService.UnitTests/Infrastructure/Security/BcryptPasswordHasherTests.cs
@@ -0,0 +1,33 @@
+﻿using PS.FreeBookHub_Lite.AuthService.Infrastructure.Security;
+
+namespace AuthService.UnitTests.Infrastructure.Security
+{
+    public class BcryptPasswordHasherTests
+    {
+        private readonly BcryptPasswordHasher _hasher = new();
+
+        [Fact]
+        public void Hash_ShouldReturn_NonEmptyString()
+        {
+            var hash = _hasher.Hash("mypassword");
+            Assert.False(string.IsNullOrWhiteSpace(hash));
+        }
+
+        [Fact]
+        public void Verify_ShouldReturn_True_ForCorrectPassword()
+        {
+            var password = "securePassword!";
+            var hash = _hasher.Hash(password);
+
+            Assert.True(_hasher.Verify(password, hash));
+        }
+
+        [Fact]
+        public void Verify_ShouldReturn_False_ForIncorrectPassword()
+        {
+            var hash = _hasher.Hash("correctPassword");
+
+            Assert.False(_hasher.Verify("wrongPassword", hash));
+        }
+    }
+}

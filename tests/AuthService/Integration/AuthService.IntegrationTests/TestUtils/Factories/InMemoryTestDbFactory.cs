259f4ebc643592a0dce5f53f10df3cac39d56e05
Alexander <sany199574@gmail.com>
2025-07-26 02:58:05 +0300

    test(auth-service): добавлены фабрики тестовых объектов в TestUtils/Factories
    - Создана структура `Factories/` для генерации `User`, `RefreshToken`, `AuthRequest`, `JwtClaims`, и других сущностей.
    - Поддержка интеграционных тестов: упрощён bootstrap тестов, снижена дублируемость.

---
+++ tests/AuthService/Integration/AuthService.IntegrationTests/TestUtils/Factories/InMemoryTestDbFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PS.FreeBookHub_Lite.AuthService.Infrastructure.Persistence;
+
+namespace AuthService.IntegrationTests.TestUtils.Factories
+{
+    public static class InMemoryTestDbFactory
+    {
+        public static AuthDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<AuthDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // уникальная база на каждый тест
+                .Options;
+
+            return new AuthDbContext(options);
+        }
+    }
+}
---
+++ tests/AuthService/Integration/AuthService.IntegrationTests/TestUtils/Factories/SqliteTestDbFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using PS.FreeBookHub_Lite.AuthService.Infrastructure.Persistence;
+
+namespace AuthService.IntegrationTests.TestUtils.Factories
+{
+    public static class SqliteTestDbFactory
+    {
+        public static AuthDbContext Create()
+        {
+            // Создаём открытое подключение SQLite In-Memory
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<AuthDbContext>()
+                .UseSqlite(connection)
+                .EnableSensitiveDataLogging() // по желанию: помогает в отладке
+                .Options;
+
+            var context = new AuthDbContext(options);
+
+            // Применяем миграции или EnsureCreated
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+}
+

847a5e18927519e49ea61a68ec8ad4607e6c413d
Alexander <sany199574@gmail.com>
2025-08-18 08:47:14 +0300

    test(cart-service): обновлён unit-тест для сущности CartItem  - добавлен тест CreateCartItem_ShouldInitializeCorrectly

--- tests/CartService/Unit/CartService.UnitTests/Domain/CartItemTests.cs
+++ tests/CartService/Unit/CartService.UnitTests/Domain/CartItemTests.cs
@@ -1,6 +1,22 @@
-﻿namespace CartService.UnitTests.Domain
+﻿using PS.FreeBookHub_Lite.CartService.Domain.Entities;
+
+namespace CartService.UnitTests.Domain
 {
     public class CartItemTests
     {
+        [Fact]
+        public void CreateCartItem_ShouldInitializeCorrectly()
+        {
+            var bookId = Guid.NewGuid();
+            var quantity = 2;
+            var unitPrice = 10m;
+
+            var item = new CartItem(bookId, quantity, unitPrice);
+
+            Assert.Equal(bookId, item.BookId);
+            Assert.Equal(quantity, item.Quantity);
+            Assert.Equal(unitPrice, item.UnitPrice);
+            Assert.Equal(quantity * unitPrice, item.TotalPrice);
+        }
     }
 }

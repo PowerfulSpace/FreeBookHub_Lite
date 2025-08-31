540a013e6941b431d526955bf4dbf74e077de23b
Alexander <sany199574@gmail.com>
2025-08-31 16:23:06 +0300

    test(auth-service): уточнена конфигурация AuthApiFactory для интеграционного окружения
    - Обновлена настройка окружения и зависимостей для тестов.

--- tests/AuthService/Integration/AuthService.IntegrationTests/TestUtils/Factories/AuthApiFactory.cs
+++ tests/AuthService/Integration/AuthService.IntegrationTests/TestUtils/Factories/AuthApiFactory.cs
@@ -8,6 +8,7 @@ using Polly;
 using PS.FreeBookHub_Lite.AuthService.Common.Interfaces.StartupTasks;
 using PS.FreeBookHub_Lite.AuthService.Infrastructure.Persistence;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Configuration;
 
 namespace AuthService.IntegrationTests.TestUtils.Factories
 {
@@ -15,26 +16,34 @@ namespace AuthService.IntegrationTests.TestUtils.Factories
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            // Добавляем конфигурацию для тестов
+            builder.ConfigureAppConfiguration(config =>
+            {
+                config.AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    ["ConnectionStrings:AuthDb"] =
+         "Server=localhost,1434;Database=AuthTest;User=sa;Password=TestPassword123!;TrustServerCertificate=true",
+                    ["Auth:JwtSettings:SecretKey"] = "my-256-bit-secret-key-is-secure!"
+                }.Select(kvp => new KeyValuePair<string, string?>(kvp.Key, kvp.Value)));
+
+            });
+
             builder.ConfigureServices(services =>
             {
-                // УДАЛЯЕМ StartupTask чтобы избежать миграций продакшен-базы
-                services.RemoveAll<IStartupTask>();
-
                 // Удаляем оригинальный DbContext
                 var descriptor = services.SingleOrDefault(
                     d => d.ServiceType == typeof(DbContextOptions<AuthDbContext>));
                 if (descriptor != null)
-                {
                     services.Remove(descriptor);
-                }
 
-                // Подключаемся к тестовой MSSQL
+                // Подключаем тестовую базу
                 services.AddDbContext<AuthDbContext>(options =>
                 {
-                    options.UseSqlServer("Server=localhost,1434;Database=AuthTest;User=sa;Password=TestPassword123!;TrustServerCertificate=true;");
+                    options.UseSqlServer(
+                        "Server=localhost,1434;Database=AuthTest;User=sa;Password=TestPassword123!;TrustServerCertificate=true");
                 });
 
-                // Применяем миграции как в продакшене
+                // Применяем миграции с retry
                 var sp = services.BuildServiceProvider();
                 using var scope = sp.CreateScope();
                 var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();

02ae785f8a7176fb15fd29425abc858e18fd76bd
Alexander <sany199574@gmail.com>
2025-09-13 07:58:36 +0300

    test(cart-service): начата реализация AddItemCommandHandlerTests
    - Добавлены первые тесты для обработчика команды AddItem.
    - Handle_NoExistingCart_ShouldCreateNewCart

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Addltem/AddItemCommandHandlerTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Addltem/AddItemCommandHandlerTests.cs
@@ -1,6 +1,47 @@
-﻿namespace PS.CartService.UnitTests.Application.CQRS.Commands.Addltem
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PS.CartService.Application.Clients;
+using PS.CartService.Application.CQRS.Commands.AddItem;
+using PS.CartService.Application.Interfaces;
+using PS.CartService.Domain.Entities;
+
+namespace PS.CartService.UnitTests.Application.CQRS.Commands.Addltem
 {
     public class AddItemCommandHandlerTests
     {
+        private readonly Mock<ICartRepository> _cartRepoMock = new();
+        private readonly Mock<IBookCatalogClient> _bookCatalogClientMock = new();
+        private readonly Mock<ILogger<AddItemCommandHandler>> _loggerMock = new();
+
+        private AddItemCommandHandler CreateHandler() =>
+            new(_cartRepoMock.Object, _bookCatalogClientMock.Object, _loggerMock.Object);
+
+
+        [Fact]
+        public async Task Handle_NoExistingCart_ShouldCreateNewCart()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var bookId = Guid.NewGuid();
+            var command = new AddItemCommand(userId, bookId, 2);
+
+            _cartRepoMock.Setup(r => r.GetCartAsync(userId, It.IsAny<CancellationToken>(), It.IsAny<bool>()))
+                         .ReturnsAsync((Cart?)null);
+
+            _bookCatalogClientMock.Setup(c => c.GetBookPriceAsync(bookId, It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync(10m);
+
+            var handler = CreateHandler();
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            Assert.Equal(Unit.Value, result);
+            _cartRepoMock.Verify(r => r.AddAsync(It.IsAny<Cart>(), It.IsAny<CancellationToken>()), Times.Once);
+            _cartRepoMock.Verify(r => r.UpdateAsync(It.IsAny<Cart>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
+
     }
 }

81c359ceaa66a7d7ebd6728c96e07910494ab06c
Alexander <sany199574@gmail.com>
2025-09-14 10:50:29 +0300

    test(cart-service): реализованы базовые тесты для AddItemCommandHandler
    - Handle_ExistingCart_ShouldUpdateCart

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Addltem/AddItemCommandHandlerTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Addltem/AddItemCommandHandlerTests.cs
@@ -43,5 +43,31 @@ namespace PS.CartService.UnitTests.Application.CQRS.Commands.Addltem
             _cartRepoMock.Verify(r => r.UpdateAsync(It.IsAny<Cart>(), It.IsAny<CancellationToken>()), Times.Never);
         }
 
+
+        [Fact]
+        public async Task Handle_ExistingCart_ShouldUpdateCart()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var bookId = Guid.NewGuid();
+            var existingCart = new Cart(userId);
+            var command = new AddItemCommand(userId, bookId, 1);
+
+            _cartRepoMock.Setup(r => r.GetCartAsync(userId, It.IsAny<CancellationToken>(), It.IsAny<bool>()))
+                         .ReturnsAsync(existingCart);
+
+            _bookCatalogClientMock.Setup(c => c.GetBookPriceAsync(bookId, It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync(20m);
+
+            var handler = CreateHandler();
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            Assert.Equal(Unit.Value, result);
+            _cartRepoMock.Verify(r => r.UpdateAsync(existingCart, It.IsAny<CancellationToken>()), Times.Once);
+            _cartRepoMock.Verify(r => r.AddAsync(It.IsAny<Cart>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
     }
 }

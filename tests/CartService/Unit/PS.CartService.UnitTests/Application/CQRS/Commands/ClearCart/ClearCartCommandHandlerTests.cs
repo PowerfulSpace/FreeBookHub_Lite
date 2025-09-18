2d3869c2baeb60d94f52abfb5a6b2fc4d8cbd373
Alexander <sany199574@gmail.com>
2025-09-18 07:16:57 +0300

    test(cart-service): реализованы тесты для ClearCartCommandHandler
    - Handle_CartNotFound_ShouldThrow

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/ClearCart/ClearCartCommandHandlerTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/ClearCart/ClearCartCommandHandlerTests.cs
@@ -1,6 +1,35 @@
-﻿namespace PS.CartService.UnitTests.Application.CQRS.Commands.ClearCart
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.CartService.Application.CQRS.Commands.ClearCart;
+using PS.CartService.Application.Interfaces;
+using PS.CartService.Domain.Entities;
+using PS.CartService.Domain.Exceptions.Cart;
+
+namespace PS.CartService.UnitTests.Application.CQRS.Commands.ClearCart
 {
     public class ClearCartCommandHandlerTests
     {
+        private readonly Mock<ICartRepository> _cartRepoMock = new();
+        private readonly Mock<ILogger<ClearCartCommandHandler>> _loggerMock = new();
+
+        private ClearCartCommandHandler CreateHandler() =>
+            new(_cartRepoMock.Object, _loggerMock.Object);
+
+
+        [Fact]
+        public async Task Handle_CartNotFound_ShouldThrow()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var command = new ClearCartCommand(userId);
+
+            _cartRepoMock.Setup(r => r.GetCartAsync(userId, It.IsAny<CancellationToken>(), It.IsAny<bool>()))
+                         .ReturnsAsync((Cart?)null);
+
+            var handler = CreateHandler();
+
+            // Act + Assert
+            await Assert.ThrowsAsync<CartNotFoundException>(() => handler.Handle(command, default));
+        }
     }
 }

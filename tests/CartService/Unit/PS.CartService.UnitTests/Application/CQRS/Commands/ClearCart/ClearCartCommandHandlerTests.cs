5904421aef8c2696e47e90e2003117cb81c5a6ee
Alexander <sany199574@gmail.com>
2025-09-19 09:32:42 +0300

    test(cart-service): реализованы тесты для ClearCartCommandHandler - Handle_CartFound_ShouldClearAndUpdate

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/ClearCart/ClearCartCommandHandlerTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/ClearCart/ClearCartCommandHandlerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Moq;
 using PS.CartService.Application.CQRS.Commands.ClearCart;
 using PS.CartService.Application.Interfaces;
@@ -30,6 +31,30 @@ namespace PS.CartService.UnitTests.Application.CQRS.Commands.ClearCart
 
             // Act + Assert
             await Assert.ThrowsAsync<CartNotFoundException>(() => handler.Handle(command, default));
+        }
+
+        [Fact]
+        public async Task Handle_CartFound_ShouldClearAndUpdate()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var cart = new Cart(userId);
+            cart.AddItem(Guid.NewGuid(), 2, 50m); // добавляем товар, чтобы потом проверить очистку
+
+            var command = new ClearCartCommand(userId);
+
+            _cartRepoMock.Setup(r => r.GetCartAsync(userId, It.IsAny<CancellationToken>(), It.IsAny<bool>()))
+                         .ReturnsAsync(cart);
+
+            var handler = CreateHandler();
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            Assert.Equal(Unit.Value, result);
+            Assert.Empty(cart.Items); // корзина должна быть очищена
+            _cartRepoMock.Verify(r => r.UpdateAsync(cart, It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }

38fcaf3566c035ed111d2fe0bf8269f56493d192
Alexander <sany199574@gmail.com>
2025-09-20 12:10:21 +0300

    test(cart-service): реализованы тесты для RemoveItemCommandHandler
     - Handle_CartNotFound_ShouldThrow

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Removeltem/RemoveItemCommandHandlerTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Removeltem/RemoveItemCommandHandlerTests.cs
@@ -1,6 +1,36 @@
-﻿namespace PS.CartService.UnitTests.Application.CQRS.Commands.Removeltem
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.CartService.Application.CQRS.Commands.RemoveItem;
+using PS.CartService.Application.Interfaces;
+using PS.CartService.Domain.Entities;
+using PS.CartService.Domain.Exceptions.Cart;
+
+namespace PS.CartService.UnitTests.Application.CQRS.Commands.Removeltem
 {
     public class RemoveItemCommandHandlerTests
     {
+        private readonly Mock<ICartRepository> _cartRepoMock = new();
+        private readonly Mock<ILogger<RemoveItemCommandHandler>> _loggerMock = new();
+
+        private RemoveItemCommandHandler CreateHandler() =>
+            new(_cartRepoMock.Object, _loggerMock.Object);
+
+        [Fact]
+        public async Task Handle_CartNotFound_ShouldThrow()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var bookId = Guid.NewGuid();
+            var command = new RemoveItemCommand(userId, bookId);
+
+            _cartRepoMock.Setup(r => r.GetCartAsync(userId, It.IsAny<CancellationToken>(), It.IsAny<bool>()))
+                         .ReturnsAsync((Cart?)null);
+
+            var handler = CreateHandler();
+
+            // Act + Assert
+            await Assert.ThrowsAsync<CartNotFoundException>(() => handler.Handle(command, default));
+        }
+
     }
 }

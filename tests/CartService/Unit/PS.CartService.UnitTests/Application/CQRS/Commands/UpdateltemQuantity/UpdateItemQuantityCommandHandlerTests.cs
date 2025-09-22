d96fdc22a9cd91b633471bf89a964ea863ee2131
Alexander <sany199574@gmail.com>
2025-09-22 21:46:08 +0300

    test(cart-service): расширены тесты для UpdateItemQuantityCommandHandler
    - Handle_CartNotFound_ShouldThrow

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/UpdateltemQuantity/UpdateItemQuantityCommandHandlerTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/UpdateltemQuantity/UpdateItemQuantityCommandHandlerTests.cs
@@ -2,6 +2,8 @@
 using Moq;
 using PS.CartService.Application.CQRS.Commands.UpdateItemQuantity;
 using PS.CartService.Application.Interfaces;
+using PS.CartService.Domain.Entities;
+using PS.CartService.Domain.Exceptions.Cart;
 
 namespace PS.CartService.UnitTests.Application.CQRS.Commands.UpdateltemQuantity
 {
@@ -12,5 +14,21 @@ namespace PS.CartService.UnitTests.Application.CQRS.Commands.UpdateltemQuantity
 
         private UpdateItemQuantityCommandHandler CreateHandler() =>
             new(_cartRepoMock.Object, _loggerMock.Object);
+
+
+        [Fact]
+        public async Task Handle_CartNotFound_ShouldThrow()
+        {
+            var userId = Guid.NewGuid();
+            var bookId = Guid.NewGuid();
+            var command = new UpdateItemQuantityCommand(userId, bookId, 5);
+
+            _cartRepoMock.Setup(r => r.GetCartAsync(userId, It.IsAny<CancellationToken>(), It.IsAny<bool>()))
+                         .ReturnsAsync((Cart?)null);
+
+            var handler = CreateHandler();
+
+            await Assert.ThrowsAsync<CartNotFoundException>(() => handler.Handle(command, default));
+        }
     }
 }

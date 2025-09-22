1cceabc0bf2f7179e56bd3f7bed160eb80a2e514
Alexander <sany199574@gmail.com>
2025-09-22 21:45:10 +0300

    test(cart-service):  Добавлены базовые настройки

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/UpdateltemQuantity/UpdateItemQuantityCommandHandlerTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/UpdateltemQuantity/UpdateItemQuantityCommandHandlerTests.cs
@@ -1,6 +1,16 @@
-﻿namespace PS.CartService.UnitTests.Application.CQRS.Commands.UpdateltemQuantity
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.CartService.Application.CQRS.Commands.UpdateItemQuantity;
+using PS.CartService.Application.Interfaces;
+
+namespace PS.CartService.UnitTests.Application.CQRS.Commands.UpdateltemQuantity
 {
     public class UpdateItemQuantityCommandHandlerTests
     {
+        private readonly Mock<ICartRepository> _cartRepoMock = new();
+        private readonly Mock<ILogger<UpdateItemQuantityCommandHandler>> _loggerMock = new();
+
+        private UpdateItemQuantityCommandHandler CreateHandler() =>
+            new(_cartRepoMock.Object, _loggerMock.Object);
     }
 }

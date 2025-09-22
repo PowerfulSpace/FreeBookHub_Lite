fd79bac841cabd95ec66d156409af1b37bfda174
Alexander <sany199574@gmail.com>
2025-09-22 07:25:44 +0300

    test(cart-service): реализованы тесты для RemoveItemCommandHandler
     - Handle_CartFound_ItemDoesNotExist_ShouldStillUpdate

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Removeltem/RemoveItemCommandHandlerTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Removeltem/RemoveItemCommandHandlerTests.cs
@@ -58,5 +58,30 @@ namespace PS.CartService.UnitTests.Application.CQRS.Commands.Removeltem
             _cartRepoMock.Verify(r => r.UpdateAsync(cart, It.IsAny<CancellationToken>()), Times.Once);
         }
 
+        [Fact]
+        public async Task Handle_CartFound_ItemDoesNotExist_ShouldStillUpdate()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var bookId = Guid.NewGuid();
+            var cart = new Cart(userId);
+            // корзина пустая
+
+            var command = new RemoveItemCommand(userId, bookId);
+
+            _cartRepoMock.Setup(r => r.GetCartAsync(userId, It.IsAny<CancellationToken>(), It.IsAny<bool>()))
+                         .ReturnsAsync(cart);
+
+            var handler = CreateHandler();
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            Assert.Equal(Unit.Value, result);
+            Assert.Empty(cart.Items); // убедимся, что корзина осталась пустой
+            _cartRepoMock.Verify(r => r.UpdateAsync(cart, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
     }
 }

40a176ba4b84d7eea04245fee775d5a5c3cf45a0
Alexander <sany199574@gmail.com>
2025-09-27 09:37:54 +0300

    test(cart-service): реализованы тесты для CheckoutCommandValidator
    - CheckoutCommandValidator

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Checkout/CheckoutCommandValidatorTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Checkout/CheckoutCommandValidatorTests.cs
@@ -1,6 +1,23 @@
-﻿namespace PS.CartService.UnitTests.Application.CQRS.Commands.Checkout
+﻿using FluentValidation.TestHelper;
+using PS.CartService.Application.CQRS.Commands.Checkout;
+
+namespace PS.CartService.UnitTests.Application.CQRS.Commands.Checkout
 {
     public class CheckoutCommandValidatorTests
     {
+        private readonly CheckoutCommandValidator _validator = new();
+
+        [Fact]
+        public void Validator_ShouldHaveError_WhenUserIdIsEmpty()
+        {
+            // Arrange
+            var command = new CheckoutCommand(Guid.Empty, "Valid address");
+
+            // Act
+            var result = _validator.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.UserId);
+        }
     }
 }

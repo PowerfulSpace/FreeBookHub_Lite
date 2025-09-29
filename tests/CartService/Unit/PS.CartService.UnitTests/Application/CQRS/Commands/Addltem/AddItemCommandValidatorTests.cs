e0c161505874ac1fd9b70fe3f9aaa7f3047731f5
Alexander <sany199574@gmail.com>
2025-09-29 07:53:38 +0300

    test(cart-service): реализованы тесты для AddItemCommandValidator
    - Validator_ShouldHaveError_WhenUserIdIsEmpty

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Addltem/AddItemCommandValidatorTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/Addltem/AddItemCommandValidatorTests.cs
@@ -1,6 +1,20 @@
-﻿namespace PS.CartService.UnitTests.Application.CQRS.Commands.Addltem
+﻿using FluentValidation.TestHelper;
+using PS.CartService.Application.CQRS.Commands.AddItem;
+
+namespace PS.CartService.UnitTests.Application.CQRS.Commands.Addltem
 {
     public class AddItemCommandValidatorTests
     {
+        private readonly AddItemCommandValidator _validator = new();
+
+        [Fact]
+        public void Validator_ShouldHaveError_WhenUserIdIsEmpty()
+        {
+            var command = new AddItemCommand(Guid.Empty, Guid.NewGuid(), 5);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId);
+        }
     }
 }

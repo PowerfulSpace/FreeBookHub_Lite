0b1d8ad2ec3c98bb48d2752aa024cfa6a7a878e1
Alexander <sany199574@gmail.com>
2025-10-05 00:03:32 +0300

    test(cart-service): Добавлен тест для UpdateItemQuantityCommandValidator
    - Validator_ShouldHaveError_WhenUserIdIsEmpty

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/UpdateltemQuantity/UpdateItemQuantityCommandValidatorTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/CQRS/Commands/UpdateltemQuantity/UpdateItemQuantityCommandValidatorTests.cs
@@ -1,6 +1,21 @@
-﻿namespace PS.CartService.UnitTests.Application.CQRS.Commands.UpdateltemQuantity
+﻿using FluentValidation.TestHelper;
+using PS.CartService.Application.CQRS.Commands.UpdateItemQuantity;
+
+namespace PS.CartService.UnitTests.Application.CQRS.Commands.UpdateltemQuantity
 {
     public class UpdateItemQuantityCommandValidatorTests
     {
+        private readonly UpdateItemQuantityCommandValidator _validator = new();
+
+        [Fact]
+        public void Validator_ShouldHaveError_WhenUserIdIsEmpty()
+        {
+            var command = new UpdateItemQuantityCommand(Guid.Empty, Guid.NewGuid(), 5);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.UserId)
+                  .WithErrorMessage("UserId is required.");
+        }
     }
 }

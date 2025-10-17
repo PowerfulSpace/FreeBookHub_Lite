c6ca559c6f2ea68c9c3a0e004748f5f53e658314
Alexander <sany199574@gmail.com>
2025-10-18 00:51:57 +0300

    test(cart-service): Добавлен тест для UpdateItemQuantityRequestValidatorTests -Validator_ShouldHaveError_WhenBookIdIsEmpty

--- tests/CartService/Unit/PS.CartService.UnitTests/Application/Validators/UpdateItemQuantityRequestValidatorTests.cs
+++ tests/CartService/Unit/PS.CartService.UnitTests/Application/Validators/UpdateItemQuantityRequestValidatorTests.cs
@@ -1,6 +1,26 @@
-﻿namespace PS.CartService.UnitTests.Application.Validators
+﻿using FluentValidation.TestHelper;
+using PS.CartService.Application.DTOs.Cart;
+using PS.CartService.Application.Validators;
+
+namespace PS.CartService.UnitTests.Application.Validators
 {
     public class UpdateItemQuantityRequestValidatorTests
     {
+        private readonly UpdateItemQuantityRequestValidator _validator = new();
+
+        [Fact]
+        public void Validator_ShouldHaveError_WhenBookIdIsEmpty()
+        {
+            var model = new UpdateItemQuantityRequest
+            {
+                BookId = Guid.Empty,
+                Quantity = 5
+            };
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(x => x.BookId);
+        }
+
     }
 }

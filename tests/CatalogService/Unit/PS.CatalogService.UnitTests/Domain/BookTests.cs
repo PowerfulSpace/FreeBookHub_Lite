ae19b20f4c0d197dce2a7c150a295da72160ac4c
Alexander <sany199574@gmail.com>
2025-10-22 21:16:06 +0300

    test(catalog-service): Добавлен тест для BookTests -CreateBook_ShouldInitializeWithValidId

---
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Domain/BookTests.cs
@@ -0,0 +1,17 @@
+﻿using PS.CatalogService.Domain.Entities;
+
+namespace PS.CatalogService.UnitTests.Domain
+{
+    public class BookTests
+    {
+        [Fact]
+        public void CreateBook_ShouldInitializeWithValidId()
+        {
+            // Act
+            var book = new Book();
+
+            // Assert
+            Assert.NotEqual(Guid.Empty, book.Id);
+        }
+    }
+}

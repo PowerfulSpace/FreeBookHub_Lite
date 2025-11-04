f9d4a1f7b9874334538aa79ec79c72d21f6ab56c
Alexander <sany199574@gmail.com>
2025-11-04 23:58:26 +0300

    test(catalog-service): Добавлен тест для UpdateBookCommandHandlerTests -Handle_BookExists_ShouldUpdateAndReturnTrue

---
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Commands/UpdateBook/UpdateBookCommandHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.CatalogService.Application.CQRS.Commands.UpdateBook;
+using PS.CatalogService.Application.Interfaces;
+using PS.CatalogService.Domain.Entities;
+using PS.CatalogService.UnitTests.Application.CQRS.Commands.DeleteBook;
+
+namespace PS.CatalogService.UnitTests.Application.CQRS.Commands.UpdateBook
+{
+    public class UpdateBookCommandHandlerTests
+    {
+        private readonly Mock<IBookRepository> _bookRepoMock = new();
+        private readonly Mock<ILogger<UpdateBookCommandHandler>> _loggerMock = new();
+
+        private UpdateBookCommandHandler CreateHandler() =>
+            new(_bookRepoMock.Object, _loggerMock.Object);
+
+        [Fact]
+        public async Task Handle_BookExists_ShouldUpdateAndReturnTrue()
+        {
+            
+            var bookId = Guid.NewGuid();
+            var existingBook = new Book
+            {
+                Id = bookId,
+                Title = "Old Title",
+                Author = "Old Author",
+                Price = 5m
+            };
+
+            var command = new UpdateBookCommand
+            {
+                Id = bookId,
+                Title = "New Title",
+                Author = "New Author",
+                Description = "Updated",
+                ISBN = "12345",
+                Price = 10m,
+                PublishedAt = DateTime.UtcNow,
+                CoverImageUrl = "http://example.com/cover.jpg"
+            };
+
+            _bookRepoMock.Setup(r => r.GetByIdAsync(bookId, It.IsAny<CancellationToken>()))
+                         .ReturnsAsync(existingBook);
+
+            _bookRepoMock.Setup(r => r.UpdateAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+                         .Returns(Task.CompletedTask);
+
+            var handler = CreateHandler();
+
+            
+            var result = await handler.Handle(command, default);
+
+            
+            Assert.True(result);
+            _bookRepoMock.Verify(r => r.UpdateAsync(It.Is<Book>(b =>
+                b.Id == bookId &&
+                b.Title == "New Title" &&
+                b.Author == "New Author" &&
+                b.Price == 10m
+            ), It.IsAny<CancellationToken>()), Times.Once);
+
+            _loggerMock.VerifyLog(LogLevel.Information, Times.AtLeast(2)); // Started + Success
+        }
+
+    }
+}

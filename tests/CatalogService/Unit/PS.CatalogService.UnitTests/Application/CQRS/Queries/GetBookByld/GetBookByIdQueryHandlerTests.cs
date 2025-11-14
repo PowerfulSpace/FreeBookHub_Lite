19cbfc7c5a2ccd354f282816f1689c293ae1daea
Alexander <sany199574@gmail.com>
2025-11-15 00:10:54 +0300

    test(catalog - service): Привидёт впорядок тест  - GetBookByIdQueryHandlerTests

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Queries/GetBookByld/GetBookByIdQueryHandlerTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Queries/GetBookByld/GetBookByIdQueryHandlerTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PS.CatalogService.UnitTests.Application.CQRS.Queries.GetBookByld
+﻿namespace PS.CatalogService.UnitTests.Application.CQRS.Queries.GetBookByld
 {
-    internal class GetBookByIdQueryHandlerTests
+    public class GetBookByIdQueryHandlerTests
     {
     }
 }

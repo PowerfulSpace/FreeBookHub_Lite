acd403501f2fc41e9cab2a52e4c8b9e06420e8f4
Alexander <sany199574@gmail.com>
2025-11-15 00:11:24 +0300

    test(catalog - service): Реализован тест  - GetBookByIdQueryHandlerTests

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Queries/GetBookByld/GetBookByIdQueryHandlerTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Queries/GetBookByld/GetBookByIdQueryHandlerTests.cs
@@ -1,6 +1,16 @@
-﻿namespace PS.CatalogService.UnitTests.Application.CQRS.Queries.GetBookByld
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.CatalogService.Application.CQRS.Queries.GetBookById;
+using PS.CatalogService.Application.Interfaces;
+
+namespace PS.CatalogService.UnitTests.Application.CQRS.Queries.GetBookByld
 {
     public class GetBookByIdQueryHandlerTests
     {
+        private readonly Mock<IBookRepository> _repositoryMock = new();
+        private readonly Mock<ILogger<GetBookByIdQueryHandler>> _loggerMock = new();
+
+        private GetBookByIdQueryHandler CreateHandler() =>
+            new(_repositoryMock.Object, _loggerMock.Object);
     }
 }

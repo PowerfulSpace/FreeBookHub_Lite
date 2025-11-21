6bb2a93d1848a18203933f0b7c9ce834c6c9f228
Alexander <sany199574@gmail.com>
2025-11-21 23:59:15 +0300

    test(catalog - service): Добавлен тест для GetBookPriceQueryHandlerTests -Handle_BookExists_ShouldReturnPrice

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Queries/GetBookPrice/GetBookPriceQueryHandlerTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Queries/GetBookPrice/GetBookPriceQueryHandlerTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using PS.CatalogService.Application.CQRS.Queries.GetBookPrice;
 using PS.CatalogService.Application.Interfaces;
+using PS.CatalogService.Domain.Entities;
 
 namespace PS.CatalogService.UnitTests.Application.CQRS.Queries.GetBookPrice
 {
@@ -12,5 +13,30 @@ namespace PS.CatalogService.UnitTests.Application.CQRS.Queries.GetBookPrice
 
         private GetBookPriceQueryHandler CreateHandler() =>
             new(_repositoryMock.Object, _loggerMock.Object);
+
+        [Fact]
+        public async Task Handle_BookExists_ShouldReturnPrice()
+        {
+
+            var id = Guid.NewGuid();
+            var book = new Book { Id = id, Price = 49.99m };
+
+            _repositoryMock
+                .Setup(r => r.GetByIdAsync(id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(book);
+
+            var handler = CreateHandler();
+            var query = new GetBookPriceQuery(id);
+
+
+            var result = await handler.Handle(query, default);
+
+
+            Assert.Equal(49.99m, result);
+
+            _repositoryMock.Verify(r => r.GetByIdAsync(id, It.IsAny<CancellationToken>()), Times.Once);
+
+            _loggerMock.VerifyLog(LogLevel.Information, Times.Exactly(2)); // Started + Success
+        }
     }
 }

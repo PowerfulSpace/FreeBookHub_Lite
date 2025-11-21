e4605fe2ae77d0c8f1afafb3145003b783764bad
Alexander <sany199574@gmail.com>
2025-11-21 09:28:58 +0300

    test(catalog - service): Добавлены зависимости для тестов  - GetBookPriceQueryHandlerTests

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Queries/GetBookPrice/GetBookPriceQueryHandlerTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Queries/GetBookPrice/GetBookPriceQueryHandlerTests.cs
@@ -1,6 +1,16 @@
-﻿namespace PS.CatalogService.UnitTests.Application.CQRS.Queries.GetBookPrice
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.CatalogService.Application.CQRS.Queries.GetBookPrice;
+using PS.CatalogService.Application.Interfaces;
+
+namespace PS.CatalogService.UnitTests.Application.CQRS.Queries.GetBookPrice
 {
     public class GetBookPriceQueryHandlerTests
     {
+        private readonly Mock<IBookRepository> _repositoryMock = new();
+        private readonly Mock<ILogger<GetBookPriceQueryHandler>> _loggerMock = new();
+
+        private GetBookPriceQueryHandler CreateHandler() =>
+            new(_repositoryMock.Object, _loggerMock.Object);
     }
 }

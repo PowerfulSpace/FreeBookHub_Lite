603850408334a69c4921d55268a1e13140fe1cf8
Alexander <sany199574@gmail.com>
2025-11-26 13:02:14 +0300

    test(catalog - service): Добавлен тест для CreateBookCommandValidatorTests -Validator_ShouldPass_WhenDataIsValid

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Commands/CreateBook/CreateBookCommandValidatorTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Commands/CreateBook/CreateBookCommandValidatorTests.cs
@@ -1,6 +1,33 @@
-﻿namespace PS.CatalogService.UnitTests.Application.CQRS.Commands.CreateBook
+﻿using FluentValidation.TestHelper;
+using PS.CatalogService.Application.CQRS.Commands.CreateBook;
+
+namespace PS.CatalogService.UnitTests.Application.CQRS.Commands.CreateBook
 {
     public class CreateBookCommandValidatorTests
     {
+        private readonly CreateBookCommandValidator _validator = new();
+
+        [Fact]
+        public void Validator_ShouldPass_WhenDataIsValid()
+        {
+
+            var command = new CreateBookCommand
+            {
+                Title = "Test Book",
+                Author = "John Doe",
+                ISBN = "978-3-16-148410-0",
+                Price = 49.99m,
+                CoverImageUrl = "https://example.com/image.jpg",
+                PublishedAt = DateTime.UtcNow,
+                Description = "Some description"
+            };
+
+
+            var result = _validator.TestValidate(command);
+
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
     }
 }

2297c38c4270abceebb44b453429966d43fdd2fc
Alexander <sany199574@gmail.com>
2025-12-06 13:34:39 +0300

    test(catalog - service): Добавлена тест для UpdateBookCommandValidatorTests -Validator_ShouldPass_WhenDataIsValid

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Commands/UpdateBook/UpdateBookCommandValidatorTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/CQRS/Commands/UpdateBook/UpdateBookCommandValidatorTests.cs
@@ -1,6 +1,31 @@
-﻿namespace PS.CatalogService.UnitTests.Application.CQRS.Commands.UpdateBook
+﻿using FluentValidation.TestHelper;
+using PS.CatalogService.Application.CQRS.Commands.UpdateBook;
+
+namespace PS.CatalogService.UnitTests.Application.CQRS.Commands.UpdateBook
 {
     public class UpdateBookCommandValidatorTests
     {
+        private readonly UpdateBookCommandValidator _validator = new();
+
+
+        [Fact]
+        public void Validator_ShouldPass_WhenDataIsValid()
+        {
+            var command = new UpdateBookCommand
+            {
+                Id = Guid.NewGuid(),
+                Title = "Updated Book",
+                Author = "John Doe",
+                ISBN = "978-3-16-148410-0",
+                Price = 29.99m,
+                CoverImageUrl = "https://example.com/book.jpg",
+                Description = "Updated description",
+                PublishedAt = DateTime.UtcNow
+            };
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
     }
 }

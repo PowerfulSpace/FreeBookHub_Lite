269a7321443802ed6083598ff986421d89905b3b
Alexander <sany199574@gmail.com>
2025-12-16 23:31:36 +0300

    test(catalog - service): Добавлена тест для CreateBookRequestValidatorTests -Validator_ShouldPass_WhenRequestIsValid

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/Validators/CreateBookRequestValidatorTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/Validators/CreateBookRequestValidatorTests.cs
@@ -1,6 +1,30 @@
-﻿namespace PS.CatalogService.UnitTests.Application.Validators
+﻿using FluentValidation.TestHelper;
+using PS.CatalogService.Application.DTOs;
+using PS.CatalogService.Application.Validators;
+
+namespace PS.CatalogService.UnitTests.Application.Validators
 {
     public class CreateBookRequestValidatorTests
     {
+        private readonly CreateBookRequestValidator _validator = new();
+
+        [Fact]
+        public void Validator_ShouldPass_WhenRequestIsValid()
+        {
+            var request = new CreateBookRequest
+            {
+                Title = "Clean Architecture",
+                Author = "Robert C. Martin",
+                Description = "Good book",
+                ISBN = "978-0134494166",
+                Price = 39.99m,
+                PublishedAt = DateTime.UtcNow,
+                CoverImageUrl = "https://example.com/cover.jpg"
+            };
+
+            var result = _validator.TestValidate(request);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
     }
 }

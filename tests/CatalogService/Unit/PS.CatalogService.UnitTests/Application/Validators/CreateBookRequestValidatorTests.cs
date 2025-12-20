93bf7cfdbcbeadc4a7ced5c37180f9ec8ea9786b
Alexander <sany199574@gmail.com>
2025-12-20 14:31:56 +0300

    test(catalog - service): Добавлена разметка для CreateBookRequestValidatorTests -Validator_ShouldHaveError_WhenPriceIsZeroOrNegative

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/Validators/CreateBookRequestValidatorTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/Validators/CreateBookRequestValidatorTests.cs
@@ -59,9 +59,13 @@ namespace PS.CatalogService.UnitTests.Application.Validators
         [Fact]
         public void Validator_ShouldHaveError_WhenPriceIsZeroOrNegative()
         {
+            // Arrange
             var request = new CreateBookRequest { Price = 0 };
 
+            // Act
             var result = _validator.TestValidate(request);
+
+            // Assert
             result.ShouldHaveValidationErrorFor(x => x.Price);
 
             request.Price = -10;

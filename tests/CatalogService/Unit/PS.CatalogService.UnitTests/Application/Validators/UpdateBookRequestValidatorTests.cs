1e89f816cb623842d55cfa1898777309df96fd87
Alexander <sany199574@gmail.com>
2025-12-27 00:05:28 +0300

    test(catalog - service): Добавлена тест для UpdateBookRequestValidatorTests -Validator_ShouldPass_WhenRequestIsValid

--- tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/Validators/UpdateBookRequestValidatorTests.cs
+++ tests/CatalogService/Unit/PS.CatalogService.UnitTests/Application/Validators/UpdateBookRequestValidatorTests.cs
@@ -1,6 +1,31 @@
-﻿namespace PS.CatalogService.UnitTests.Application.Validators
+﻿using FluentValidation.TestHelper;
+using PS.CatalogService.Application.DTOs;
+using PS.CatalogService.Application.Validators;
+
+namespace PS.CatalogService.UnitTests.Application.Validators
 {
     public class UpdateBookRequestValidatorTests
     {
+        private readonly UpdateBookRequestValidator _validator = new();
+
+
+        [Fact]
+        public void Validator_ShouldPass_WhenRequestIsValid()
+        {
+            var request = new UpdateBookRequest
+            {
+                Title = "Domain-Driven Design",
+                Author = "Eric Evans",
+                Description = "DDD book",
+                ISBN = "978-0321125217",
+                Price = 49.99m,
+                PublishedAt = DateTime.UtcNow,
+                CoverImageUrl = "https://example.com/cover.jpg"
+            };
+
+            var result = _validator.TestValidate(request);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
     }
 }

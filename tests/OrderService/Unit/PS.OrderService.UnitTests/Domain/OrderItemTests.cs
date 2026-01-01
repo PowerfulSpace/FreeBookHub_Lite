b9f4bfbbdef1666a51ff858546370fc974d79ce7
Alexander <sany199574@gmail.com>
2026-01-02 01:10:54 +0300

    test(order - service): Добавлена тест для OrderItemTests -CreateOrderItem_ShouldInitializeCorrectly

---
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Domain/OrderItemTests.cs
@@ -0,0 +1,23 @@
+﻿using PS.OrderService.Domain.Entities;
+
+namespace PS.OrderService.UnitTests.Domain
+{
+    public class OrderItemTests
+    {
+        [Fact]
+        public void CreateOrderItem_ShouldInitializeCorrectly()
+        {
+            var bookId = Guid.NewGuid();
+            var price = 25m;
+            var quantity = 2;
+
+            var item = new OrderItem(bookId, price, quantity);
+
+            Assert.Equal(bookId, item.BookId);
+            Assert.Equal(price, item.UnitPrice);
+            Assert.Equal(quantity, item.Quantity);
+            Assert.Equal(50m, item.TotalPrice);
+        }
+
+    }
+}

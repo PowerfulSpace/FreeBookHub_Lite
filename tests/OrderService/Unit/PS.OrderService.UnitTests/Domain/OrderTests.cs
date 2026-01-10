235e98eea27a14e27eef6f423c4a802e705a8b47
Alexander <sany199574@gmail.com>
2026-01-10 03:28:23 +0300

    test(order - service): Добавлена тест для OrderTests -CreateOrder_ShouldInitializeCorrectly

---
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Domain/OrderTests.cs
@@ -0,0 +1,24 @@
+﻿using PS.OrderService.Domain.Entities;
+using PS.OrderService.Domain.Enums;
+
+namespace PS.OrderService.UnitTests.Domain
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void CreateOrder_ShouldInitializeCorrectly()
+        {
+            var userId = Guid.NewGuid();
+            var address = "Some address";
+
+            var order = new Order(userId, address);
+
+            Assert.NotEqual(Guid.Empty, order.Id);
+            Assert.Equal(userId, order.UserId);
+            Assert.Equal(address, order.ShippingAddress);
+            Assert.Equal(OrderStatus.New, order.Status);
+            Assert.True(order.CreatedAt <= DateTime.UtcNow);
+            Assert.Empty(order.Items);
+        }
+    }
+}

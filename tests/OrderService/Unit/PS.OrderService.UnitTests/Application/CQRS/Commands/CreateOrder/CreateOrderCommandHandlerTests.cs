f6a389f0d48ae4ba5909b3b50dc752c1dd906e60
Alexander <sany199574@gmail.com>
2026-02-01 00:22:43 +0300

    test(order- service): Добавлен метод для CreateOrderCommandHandlerTests -CreateHandler

---
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Commands/CreateOrder/CreateOrderCommandHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using PS.OrderService.Application.CQRS.Commands.CreateOrder;
+using PS.OrderService.Application.Interfaces;
+using PS.OrderService.Common.Configuration;
+using PS.OrderService.Common.Events.Interfaces;
+
+namespace PS.OrderService.UnitTests.Application.CQRS.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandlerTests
+    {
+        private readonly Mock<IOrderRepository> _orderRepositoryMock = new();
+        private readonly Mock<IEventPublisher> _eventPublisherMock = new();
+        private readonly Mock<ILogger<CreateOrderCommandHandler>> _loggerMock = new();
+        private readonly Mock<IOptions<RabbitMqConfig>> _optionsMock = new();
+
+        private CreateOrderCommandHandler CreateHandler()
+        {
+            _optionsMock.Setup(o => o.Value)
+                .Returns(new RabbitMqConfig
+                {
+                    RoutingKeys = new RabbitMqConfig.RoutingKeysConfig
+                    {
+                        OrderCreatedRoutingKey = "order.created"
+                    }
+                });
+
+            return new CreateOrderCommandHandler(
+                _orderRepositoryMock.Object,
+                _loggerMock.Object,
+                _eventPublisherMock.Object,
+                _optionsMock.Object);
+        }
+
+    }
+}

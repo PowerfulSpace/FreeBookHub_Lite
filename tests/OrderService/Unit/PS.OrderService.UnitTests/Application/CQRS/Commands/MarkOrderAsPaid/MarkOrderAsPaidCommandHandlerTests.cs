0b8be3841ab8dd9e70f2c1caf6c788d0827af63a
Alexander <sany199574@gmail.com>
2026-02-10 00:06:41 +0300

    test(order - service): Добавлен тест для MarkOrderAsPaidCommandHandlerTests -Handle_ShouldMarkOrderAsPaid_WhenOrderExists

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Commands/MarkOrderAsPaid/MarkOrderAsPaidCommandHandlerTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Commands/MarkOrderAsPaid/MarkOrderAsPaidCommandHandlerTests.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Moq;
 using PS.OrderService.Application.CQRS.Commands.MarkOrderAsPaid;
 using PS.OrderService.Application.Interfaces;
+using PS.OrderService.Domain.Entities;
 
 namespace PS.OrderService.UnitTests.Application.CQRS.Commands.MarkOrderAsPaid
 {
@@ -18,5 +20,32 @@ namespace PS.OrderService.UnitTests.Application.CQRS.Commands.MarkOrderAsPaid
                 _orderRepositoryMock.Object,
                 _loggerMock.Object);
         }
+
+        [Fact]
+        public async Task Handle_ShouldMarkOrderAsPaid_WhenOrderExists()
+        {
+            var orderId = Guid.NewGuid();
+            var orderMock = new Mock<Order>();
+
+            _orderRepositoryMock
+                .Setup(r => r.GetByIdAsync(
+                    orderId,
+                    It.IsAny<CancellationToken>(),
+                    It.IsAny<bool>()))
+                .ReturnsAsync(orderMock.Object);
+
+            var command = new MarkOrderAsPaidCommand(orderId);
+
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            Assert.Equal(Unit.Value, result);
+
+            orderMock.Verify(o => o.MarkAsPaid(), Times.Once);
+
+            _orderRepositoryMock.Verify(r =>
+                r.UpdateAsync(orderMock.Object, It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
     }
 }

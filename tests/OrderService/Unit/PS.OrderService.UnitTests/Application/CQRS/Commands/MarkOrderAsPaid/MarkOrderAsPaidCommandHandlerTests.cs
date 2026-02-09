fa35a7f99a1f1e40f435f67e4c21c0e27c477a2c
Alexander <sany199574@gmail.com>
2026-02-10 00:05:31 +0300

    test(order - service): Добавлен поля для класса  - MarkOrderAsPaidCommandHandlerTests

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Commands/MarkOrderAsPaid/MarkOrderAsPaidCommandHandlerTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Commands/MarkOrderAsPaid/MarkOrderAsPaidCommandHandlerTests.cs
@@ -1,6 +1,16 @@
-﻿namespace PS.OrderService.UnitTests.Application.CQRS.Commands.MarkOrderAsPaid
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.OrderService.Application.CQRS.Commands.MarkOrderAsPaid;
+using PS.OrderService.Application.Interfaces;
+
+namespace PS.OrderService.UnitTests.Application.CQRS.Commands.MarkOrderAsPaid
 {
     public class MarkOrderAsPaidCommandHandlerTests
     {
+        private readonly Mock<IOrderRepository> _orderRepositoryMock = new();
+        private readonly Mock<ILogger<MarkOrderAsPaidCommandHandler>> _loggerMock = new();
+        private readonly MarkOrderAsPaidCommandHandler _handler;
+
+
     }
 }

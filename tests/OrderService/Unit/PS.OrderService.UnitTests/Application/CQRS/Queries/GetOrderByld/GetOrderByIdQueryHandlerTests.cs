e1005251c1f2364d0931d3925a706652a04ba184
Alexander <sany199574@gmail.com>
2026-02-14 00:07:28 +0300

    test(order - service): Добавлен класс для тестов  - GetOrderByIdQueryHandlerTests

---
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Queries/GetOrderByld/GetOrderByIdQueryHandlerTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.OrderService.Application.CQRS.Queries.GetOrderById;
+using PS.OrderService.Application.Interfaces;
+
+namespace PS.OrderService.UnitTests.Application.CQRS.Queries.GetOrderByld
+{
+    public class GetOrderByIdQueryHandlerTests
+    {
+        private readonly Mock<IOrderRepository> _orderRepositoryMock = new();
+        private readonly Mock<ILogger<GetOrderByIdQueryHandler>> _loggerMock = new();
+
+        private GetOrderByIdQueryHandler CreateHandler() =>
+            new(_orderRepositoryMock.Object, _loggerMock.Object);
+    }
+}

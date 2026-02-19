72a4f19977863631d23248d26efbc83048626431
Alexander <sany199574@gmail.com>
2026-02-19 07:06:23 +0300

    test(order - service): Добавлен класс для тестов  - GetAllOrdersByUserIdQueryHandlerTests

---
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Queries/GetAllOrdersByUserld/GetAllOrdersByUserIdQueryHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.OrderService.UnitTests.Application.CQRS.Queries.GetAllOrdersByUserld
+{
+    internal class GetAllOrdersByUserIdQueryHandlerTests
+    {
+    }
+}

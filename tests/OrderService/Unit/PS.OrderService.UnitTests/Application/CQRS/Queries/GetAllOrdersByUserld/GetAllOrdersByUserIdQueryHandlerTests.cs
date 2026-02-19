f9d72f4b8a376438bbbfd86e57a8acb99af9cc5e
Alexander <sany199574@gmail.com>
2026-02-19 07:06:53 +0300

    test(order - service): привидён в порядок  класс для тестов  - GetAllOrdersByUserIdQueryHandlerTests

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Queries/GetAllOrdersByUserld/GetAllOrdersByUserIdQueryHandlerTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Queries/GetAllOrdersByUserld/GetAllOrdersByUserIdQueryHandlerTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PS.OrderService.UnitTests.Application.CQRS.Queries.GetAllOrdersByUserld
+﻿namespace PS.OrderService.UnitTests.Application.CQRS.Queries.GetAllOrdersByUserld
 {
-    internal class GetAllOrdersByUserIdQueryHandlerTests
+    public class GetAllOrdersByUserIdQueryHandlerTests
     {
     }
 }

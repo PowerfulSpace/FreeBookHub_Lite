d7f5c878eae2cf79b0aa46b79de09a4098c6fdd9
Alexander <sany199574@gmail.com>
2026-02-19 07:08:39 +0300

    test(order - service): Добавлен тест для GetAllOrdersByUserIdQueryHandlerTests -Handle_UserHasOrders_ShouldReturnOrderResponses

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Queries/GetAllOrdersByUserld/GetAllOrdersByUserIdQueryHandlerTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Queries/GetAllOrdersByUserld/GetAllOrdersByUserIdQueryHandlerTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using PS.OrderService.Application.CQRS.Queries.GetAllOrdersByUserId;
 using PS.OrderService.Application.Interfaces;
+using PS.OrderService.Domain.Entities;
 
 namespace PS.OrderService.UnitTests.Application.CQRS.Queries.GetAllOrdersByUserld
 {
@@ -12,5 +13,34 @@ namespace PS.OrderService.UnitTests.Application.CQRS.Queries.GetAllOrdersByUserl
 
         private GetAllOrdersByUserIdQueryHandler CreateHandler() =>
             new(_orderRepositoryMock.Object, _loggerMock.Object);
+
+
+        [Fact]
+        public async Task Handle_UserHasOrders_ShouldReturnOrderResponses()
+        {
+            var userId = Guid.NewGuid();
+
+            var orders = new List<Order>
+            {
+                new Order(userId, "Address 1"),
+                new Order(userId, "Address 2")
+            };
+
+            _orderRepositoryMock
+                .Setup(r => r.GetAllByUserIdAsync(userId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(orders);
+
+            var handler = CreateHandler();
+            var query = new GetAllOrdersByUserIdQuery(userId);
+
+            var result = await handler.Handle(query, default);
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+
+            _orderRepositoryMock.Verify(r =>
+                r.GetAllByUserIdAsync(userId, It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
     }
 }

e191556cdc423f279aa3a27239cdcb1da7370c89
Alexander <sany199574@gmail.com>
2026-02-19 07:07:47 +0300

    test(order - service): Добавлено состаяние для класс - GetAllOrdersByUserIdQueryHandlerTests

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Queries/GetAllOrdersByUserld/GetAllOrdersByUserIdQueryHandlerTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Application/CQRS/Queries/GetAllOrdersByUserld/GetAllOrdersByUserIdQueryHandlerTests.cs
@@ -1,6 +1,16 @@
-﻿namespace PS.OrderService.UnitTests.Application.CQRS.Queries.GetAllOrdersByUserld
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.OrderService.Application.CQRS.Queries.GetAllOrdersByUserId;
+using PS.OrderService.Application.Interfaces;
+
+namespace PS.OrderService.UnitTests.Application.CQRS.Queries.GetAllOrdersByUserld
 {
     public class GetAllOrdersByUserIdQueryHandlerTests
     {
+        private readonly Mock<IOrderRepository> _orderRepositoryMock = new();
+        private readonly Mock<ILogger<GetAllOrdersByUserIdQueryHandler>> _loggerMock = new();
+
+        private GetAllOrdersByUserIdQueryHandler CreateHandler() =>
+            new(_orderRepositoryMock.Object, _loggerMock.Object);
     }
 }

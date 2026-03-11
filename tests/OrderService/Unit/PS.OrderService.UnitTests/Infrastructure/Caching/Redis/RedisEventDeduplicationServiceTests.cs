d311f93f9d6c311715030abb70e1964cf906d8c5
Alexander <sany199574@gmail.com>
2026-03-11 05:23:11 +0300

    test(order - service): Добавлен пустой тест  - RedisEventDeduplicationServiceTests

---
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Caching/Redis/RedisEventDeduplicationServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.OrderService.UnitTests.Infrastructure.Caching.Redis
+{
+    internal class RedisEventDeduplicationServiceTests
+    {
+    }
+}

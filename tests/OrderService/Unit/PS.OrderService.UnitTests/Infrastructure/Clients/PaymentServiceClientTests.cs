eedebc348754a69d635e794433670349b03f1f87
Alexander <sany199574@gmail.com>
2026-03-18 01:37:54 +0300

    test(order - service):  Привидён в подготовку  - PaymentServiceClientTests

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Clients/PaymentServiceClientTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Clients/PaymentServiceClientTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PS.OrderService.UnitTests.Infrastructure.Clients
+﻿namespace PS.OrderService.UnitTests.Infrastructure.Clients
 {
-    internal class PaymentServiceClientTests
+    public class PaymentServiceClientTests
     {
     }
 }

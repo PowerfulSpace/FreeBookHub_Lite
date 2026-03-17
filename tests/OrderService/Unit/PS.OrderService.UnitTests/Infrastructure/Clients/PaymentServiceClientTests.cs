2adc2b341721c42d946e50e179e69a01a7f9852f
Alexander <sany199574@gmail.com>
2026-03-18 01:38:55 +0300

    test(order - service): Добавлена конструкция для инитиацизации

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Clients/PaymentServiceClientTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Clients/PaymentServiceClientTests.cs
@@ -1,6 +1,32 @@
-﻿namespace PS.OrderService.UnitTests.Infrastructure.Clients
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.Protected;
+using PS.OrderService.Infrastructure.Clients;
+
+namespace PS.OrderService.UnitTests.Infrastructure.Clients
 {
     public class PaymentServiceClientTests
     {
+        private readonly Mock<ILogger<PaymentServiceClient>> _loggerMock = new();
+
+        private PaymentServiceClient CreateClient(HttpResponseMessage response)
+        {
+            var handlerMock = new Mock<HttpMessageHandler>();
+
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    It.IsAny<HttpRequestMessage>(),
+                    It.IsAny<CancellationToken>())
+                .ReturnsAsync(response);
+
+            var httpClient = new HttpClient(handlerMock.Object)
+            {
+                BaseAddress = new Uri("http://localhost")
+            };
+
+            return new PaymentServiceClient(httpClient, _loggerMock.Object);
+        }
     }
 }

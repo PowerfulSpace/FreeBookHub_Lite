a4a2e7a77652d3f28f74d31efcc022a8073a9cf6
Alexander <sany199574@gmail.com>
2026-03-22 07:09:24 +0300

    test(order - service): Добавлен InternalAuthHandlerTests - методо для пораждения клиентов

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Http/Handlers/InternalAuthHandlerTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Http/Handlers/InternalAuthHandlerTests.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using Moq.Protected;
+using PS.OrderService.Infrastructure.Http.Handlers;
+using System.Net;
 
 namespace PS.OrderService.UnitTests.Infrastructure.Http.Handlers
 {
-    internal class InternalAuthHandlerTests
+    public class InternalAuthHandlerTests
     {
+        private HttpClient CreateClient(string? secretKey, out HttpRequestMessage? capturedRequest)
+        {
+            HttpRequestMessage? localRequest = null;
+            capturedRequest = null;
+
+            var configMock = new Mock<IConfiguration>();
+            configMock.Setup(c => c["InternalApi:SecretKey"]).Returns(secretKey);
+
+            var innerHandlerMock = new Mock<HttpMessageHandler>();
+
+            innerHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    It.IsAny<HttpRequestMessage>(),
+                    It.IsAny<CancellationToken>())
+                .Callback<HttpRequestMessage, CancellationToken>((req, _) =>
+                {
+                    localRequest = req;
+                })
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK));
+
+            var handler = new InternalAuthHandler(configMock.Object)
+            {
+                InnerHandler = innerHandlerMock.Object
+            };
+
+            var client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri("http://localhost")
+            };
+
+            capturedRequest = localRequest;
+            return client;
+        }
     }
 }

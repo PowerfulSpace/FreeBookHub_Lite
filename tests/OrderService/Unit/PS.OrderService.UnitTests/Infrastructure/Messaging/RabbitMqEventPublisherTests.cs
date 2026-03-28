7b312ef430d8fa1398b3c3c5a3afd0f297065479
Alexander <sany199574@gmail.com>
2026-03-29 00:08:51 +0300

    test(order - service): Добавлен Вспомогательный класс  - TestableRabbitMqEventPublisher

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Messaging/RabbitMqEventPublisherTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Messaging/RabbitMqEventPublisherTests.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using PS.OrderService.Common.Configuration;
+using PS.OrderService.Infrastructure.Messaging;
+using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PS.OrderService.UnitTests.Infrastructure.Messaging
 {
-    internal class RabbitMqEventPublisherTests
+    public class RabbitMqEventPublisherTests
     {
+
+        
+    }
+
+
+    internal class TestableRabbitMqEventPublisher : RabbitMqEventPublisher
+    {
+        public TestableRabbitMqEventPublisher(
+            ILogger<RabbitMqEventPublisher> logger,
+            IOptions<RabbitMqConfig> config,
+            IConnection connection,
+            IModel channel)
+            : base(logger, config)
+        {
+            typeof(RabbitMqEventPublisher)
+                .GetField("_connection", BindingFlags.NonPublic | BindingFlags.Instance)!
+                .SetValue(this, connection);
+
+            typeof(RabbitMqEventPublisher)
+                .GetField("_channel", BindingFlags.NonPublic | BindingFlags.Instance)!
+                .SetValue(this, channel);
+        }
     }
 }

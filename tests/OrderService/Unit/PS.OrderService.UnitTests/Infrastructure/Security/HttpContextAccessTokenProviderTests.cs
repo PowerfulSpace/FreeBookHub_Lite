1e12c04cf4e65473ac493a560b783c7fab3f85e4
Alexander <sany199574@gmail.com>
2026-04-03 00:01:59 +0300

    test(order - service): Добавлен тест для HttpContextAccessTokenProviderTests -GetAccessToken_ShouldReturnToken_WhenAuthorizationHeaderExists

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Security/HttpContextAccessTokenProviderTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/Security/HttpContextAccessTokenProviderTests.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using PS.OrderService.Infrastructure.Security;
 
 namespace PS.OrderService.UnitTests.Infrastructure.Security
 {
-    internal class HttpContextAccessTokenProviderTests
+    public class HttpContextAccessTokenProviderTests
     {
+        private readonly Mock<IHttpContextAccessor> _httpContextAccessorMock;
+        private readonly HttpContextAccessTokenProvider _provider;
+
+        public HttpContextAccessTokenProviderTests()
+        {
+            _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            _provider = new HttpContextAccessTokenProvider(_httpContextAccessorMock.Object);
+        }
+
+        [Fact]
+        public void GetAccessToken_ShouldReturnToken_WhenAuthorizationHeaderExists()
+        {
+            var context = new DefaultHttpContext();
+            context.Request.Headers["Authorization"] = "Bearer test-token";
+
+            _httpContextAccessorMock
+                .Setup(x => x.HttpContext)
+                .Returns(context);
+
+            var result = _provider.GetAccessToken();
+
+            Assert.Equal("Bearer test-token", result);
+        }
+
     }
 }

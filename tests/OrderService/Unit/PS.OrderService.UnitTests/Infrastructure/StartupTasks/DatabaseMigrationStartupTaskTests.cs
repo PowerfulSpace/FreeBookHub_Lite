e74934c0d3d8fb9b6056e6b1f1f0e57af4092652
Alexander <sany199574@gmail.com>
2026-04-10 00:16:01 +0300

    test(order - service): Добавлен тест для DatabaseMigrationStartupTaskTests Добавлен пакет - Microsoft.EntityFrameworkCore.InMemory

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/StartupTasks/DatabaseMigrationStartupTaskTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/StartupTasks/DatabaseMigrationStartupTaskTests.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using PS.OrderService.Infrastructure.Persistence;
+using PS.OrderService.Infrastructure.StartupTasks;
 
 namespace PS.OrderService.UnitTests.Infrastructure.StartupTasks
 {
     public class DatabaseMigrationStartupTaskTests
     {
-       
+        
     }
 }

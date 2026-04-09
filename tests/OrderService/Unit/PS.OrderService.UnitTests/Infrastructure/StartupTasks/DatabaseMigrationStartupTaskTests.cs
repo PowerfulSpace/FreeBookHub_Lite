410021f7202a7aa0243675cf1dc45fe92d118017
Alexander <sany199574@gmail.com>
2026-04-10 00:17:28 +0300

    test(order - service): Добавлен тест для DatabaseMigrationStartupTaskTests -ExecuteAsync_ShouldCompleteSuccessfully

--- tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/StartupTasks/DatabaseMigrationStartupTaskTests.cs
+++ tests/OrderService/Unit/PS.OrderService.UnitTests/Infrastructure/StartupTasks/DatabaseMigrationStartupTaskTests.cs
@@ -7,7 +7,22 @@ namespace PS.OrderService.UnitTests.Infrastructure.StartupTasks
 {
     public class DatabaseMigrationStartupTaskTests
     {
-        
+        [Fact]
+        public async Task ExecuteAsync_ShouldCompleteSuccessfully()
+        {
+            var services = new ServiceCollection();
+
+            services.AddDbContext<OrderDbContext>(options =>
+                options.UseInMemoryDatabase("T"));
+
+            var provider = services.BuildServiceProvider();
+
+            var task = new TestableDatabaseMigrationStartupTask(provider);
+
+            var exception = await Record.ExceptionAsync(() => task.ExecutePublicAsync());
+
+            Assert.Null(exception);
+        }
     }
 
     public class TestableDatabaseMigrationStartupTask : DatabaseMigrationStartupTask

6eb9fb1ea96e7d409cf43be66126d3975bb0afc3
Alexander <sany199574@gmail.com>
2026-04-17 00:00:13 +0300

    test(payment  - service): Добавлен тест для PaymentTests -Constructor_ShouldCreatePayment_WhenDataIsValid

---
+++ tests/PaymentService/Unit/PS.PaymentService.UnitTests/Domain/PaymentTests.cs
@@ -0,0 +1,26 @@
+﻿using PS.PaymentService.Domain.Entities;
+using PS.PaymentService.Domain.Enums;
+
+namespace PS.PaymentService.UnitTests.Domain
+{
+    public class PaymentTests
+    {
+        [Fact]
+        public void Constructor_ShouldCreatePayment_WhenDataIsValid()
+        {
+            var orderId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+            var amount = 100m;
+
+            var payment = new Payment(orderId, userId, amount);
+
+            Assert.Equal(orderId, payment.OrderId);
+            Assert.Equal(userId, payment.UserId);
+            Assert.Equal(amount, payment.Amount);
+            Assert.Equal(PaymentStatus.Pending, payment.Status);
+            Assert.NotEqual(Guid.Empty, payment.Id);
+            Assert.True(payment.CreatedAt <= DateTime.UtcNow);
+        }
+
+    }
+}

307a4c32632f2d90d5f5df40bcf81f85758949d9
Alexander <sany199574@gmail.com>
2026-05-02 00:16:19 +0300

    test(payment - service):  Подготовлен класс тестда для  - GetPaymentByIdQueryHandlerTests

--- tests/PaymentService/Unit/PS.PaymentService.UnitTests/Application/CQRS/Queries/GetPaymentById/GetPaymentByIdQueryHandlerTests.cs
+++ tests/PaymentService/Unit/PS.PaymentService.UnitTests/Application/CQRS/Queries/GetPaymentById/GetPaymentByIdQueryHandlerTests.cs
@@ -1,6 +1,25 @@
-﻿namespace PS.PaymentService.UnitTests.Application.CQRS.Queries.GetPaymentById
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.PaymentService.Application.CQRS.Queries.GetPaymentById;
+using PS.PaymentService.Application.Interfaces;
+
+namespace PS.PaymentService.UnitTests.Application.CQRS.Queries.GetPaymentById
 {
     public class GetPaymentByIdQueryHandlerTests
     {
+        private readonly Mock<IPaymentRepository> _repoMock;
+        private readonly Mock<ILogger<GetPaymentByIdQueryHandler>> _loggerMock;
+        private readonly GetPaymentByIdQueryHandler _handler;
+
+        public GetPaymentByIdQueryHandlerTests()
+        {
+            _repoMock = new Mock<IPaymentRepository>();
+            _loggerMock = new Mock<ILogger<GetPaymentByIdQueryHandler>>();
+
+            _handler = new GetPaymentByIdQueryHandler(
+                _repoMock.Object,
+                _loggerMock.Object);
+        }
+
     }
 }

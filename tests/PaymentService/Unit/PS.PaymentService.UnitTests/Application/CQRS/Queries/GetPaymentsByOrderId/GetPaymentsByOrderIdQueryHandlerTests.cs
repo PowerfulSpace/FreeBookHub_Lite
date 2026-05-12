1703204619e457801ffcb5b9d0a2ba0603797c8c
Alexander <sany199574@gmail.com>
2026-05-12 05:32:53 +0300

    test(payment  - service): Добавлен класс для тестов  - GetPaymentsByOrderIdQueryHandlerTests

---
+++ tests/PaymentService/Unit/PS.PaymentService.UnitTests/Application/CQRS/Queries/GetPaymentsByOrderId/GetPaymentsByOrderIdQueryHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PS.PaymentService.Application.CQRS.Queries.GetPaymentsByOrderId;
+using PS.PaymentService.Application.Interfaces;
+
+namespace PS.PaymentService.UnitTests.Application.CQRS.Queries.GetPaymentsByOrderId
+{
+    public class GetPaymentsByOrderIdQueryHandlerTests
+    {
+        private readonly Mock<IPaymentRepository> _paymentRepositoryMock;
+        private readonly Mock<ILogger<GetPaymentsByOrderIdQueryHandler>> _loggerMock;
+        private readonly GetPaymentsByOrderIdQueryHandler _handler;
+
+        public GetPaymentsByOrderIdQueryHandlerTests()
+        {
+            _paymentRepositoryMock = new Mock<IPaymentRepository>();
+            _loggerMock = new Mock<ILogger<GetPaymentsByOrderIdQueryHandler>>();
+
+            _handler = new GetPaymentsByOrderIdQueryHandler(
+                _paymentRepositoryMock.Object,
+                _loggerMock.Object);
+        }
+    }
+}
